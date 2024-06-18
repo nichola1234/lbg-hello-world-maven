6aad315406f516dbc241ad927831112809766e7e
NicholaR <sticknjr@gmail.com>
2024-06-18 16:18:11 +0100

    added hello Julie

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Nichola"));
+         System.out.println(sayHelloToSomeone("Julie"));
         System.out.println(sayGoodbye());
     }
 
